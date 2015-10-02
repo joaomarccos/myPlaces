a279e4d6cbb32eac109e11950d17803d5042a296
joaomarccos <joaomarccos.ads@gmail.com>
2015-10-01 22:44:25 -0300

    Removendo Place do banco relacional

--- src/main/java/br/edu/ifpb/db/myplaces/entitys/Place.java
+++ src/main/java/br/edu/ifpb/db/myplaces/entitys/Place.java
@@ -1,20 +1,14 @@
 package br.edu.ifpb.db.myplaces.entitys;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import org.bson.Document;
 
 /**
  *
  * @author João Marcos F <joaomarccos.ads@gmail.com>
  */
-@Entity
 public class Place implements Serializable{
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+   
     private int id;
     private String description;
     private double lat;
