d6b3b0d2cf698964995e55d33508bbf13af7dc74
joaomarccos <joaomarccos.ads@gmail.com>
2015-10-04 00:04:53 -0300

    Filtro de autenticação de usuário renomeado

--- src/main/java/br/edu/ifpb/db/myplaces/filters/UserAuth.java
+++ src/main/java/br/edu/ifpb/db/myplaces/filters/UserAuth.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.ifpb.db.myplaces.servlets;
+package br.edu.ifpb.db.myplaces.filters;
 
 import java.io.IOException;
 import javax.servlet.Filter;
